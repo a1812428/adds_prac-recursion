f40d5a6e5df057a571c3e0fd35bea7ef8a7d666f
a1812428 <100541936+a1812428@users.noreply.github.com>
2022-08-23 22:18:52 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -5,18 +5,62 @@ using namespace std;
 
 int main()
 {
-    int num;
-    cin >> num;
-    string str;
-    cin >> str;
-    int numCrates;
-    cin >> numCrates;
-    int loadSize;
-    cin >> loadSize;
+    int ans1, ans3 = -1, ans4 = -1;
+    string ans2;
     Reverse *r = new Reverse();
     Truckloads *t = new Truckloads();
     EfficientTruckloads *truck = new EfficientTruckloads();
-    cout << r->reverseDigit(num) << " " << r->reverseString(str) << " " << t->numTrucks(numCrates, loadSize) << " " << truck->numTrucks(numCrates, loadSize);
+    int num;
+    if (cin >> num)
+    {
+        ans1 = r->reverseDigit(num);
+    }
+    else
+    {
+        ans1 = -1;
+        cout << "Hello";
+    }
+    string str;
+    if (cin >> str)
+    {
+        ans2 = r->reverseString(str);
+    }
+    else
+    {
+        ans2 = "ERROR";
+    }
+    int numCrates;
+    int loadSize;
+    if (cin >> numCrates)
+    {
+    }
+    else
+    {
+        ans3 = -1;
+        ans4 = -1;
+    }
+    if (cin >> loadSize)
+    {
+        ans3 = t->numTrucks(numCrates, loadSize);
+        ans4 = truck->numTrucks(numCrates, loadSize);
+    }
+    else
+    {
+        ans3 = -1;
+        ans4 = -1;
+    }
+
+    cout << ans1 << " " << ans2 << " ";
+    if (ans3 == -1)
+    {
+        cout << "ERROR"
+             << " "
+             << "ERROR";
+    }
+    else
+    {
+        cout << ans3 << " " << ans4;
+    }
 
     return 0;
 }
